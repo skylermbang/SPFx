78772555db6d51e635de19274bd5d97e22a943e4
Skylermbang <bang618@hotmail.com>
2024-11-27 13:34:44 +1100

    fundamental lecture 1

---
+++ helloWorld2/src/webparts/helloWorld/loc/mystrings.d.ts
@@ -0,0 +1,20 @@
+declare interface IHelloWorldWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+  MyCustomeWelcomeText:string;
+}
+
+declare module 'HelloWorldWebPartStrings' {
+  const strings: IHelloWorldWebPartStrings;
+  export = strings;
+}
